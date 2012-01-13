be590ca433dd83d23530460494aaf7eae44c455d
huoxito <huoxito@gmail.com>
2012-01-13 01:14:17 -0300

    mudo estilo do botao selecionado

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ $(document).ready(function(){
     setPalavra(configs);
 
     $('.nivel').attr('disabled','disabled');
-    $('#nivelinfo').html('Nivel ' + nivel);
+    $(this).addClass('info');
 
     loadImages();
 
